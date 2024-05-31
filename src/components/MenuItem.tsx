4164cc8c58b606e1e5d5ae9347b88caddf4f491b
binhpham199x <binhpham199x@gmail.com>
2024-05-31 21:29:43 +0200

    feat: adjust MenuItem property type to interface

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import React from "react";
 import { IconType } from "react-icons";
 
-type Props = {
-   title: string;
-   address: string;
-   Icon: IconType;
-};
+interface Props {
+	title: string;
+	address: string;
+	Icon: IconType;
+}
 
-const MenuItem = (props: Props) => {
-   return (
-      <Link href={props.address} className="hover:text-amber-500">
-         <props.Icon className="text-2xl sm:hidden"/>
-         <p className="uppercase hidden sm:inline text-sm">{props.title}</p>
-      </Link>
-   );
+const MenuItem = ({ title, address, Icon }: Props) => {
+	return (
+		<Link href={address} className="hover:text-amber-500">
+			<Icon className="text-2xl sm:hidden" />
+			<p className="uppercase hidden sm:inline text-sm">{title}</p>
+		</Link>
+	);
 };
 
 export default MenuItem;
