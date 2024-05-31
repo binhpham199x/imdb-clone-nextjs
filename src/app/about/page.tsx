d421438fe6de048880d92896303c7b97db47734b
binhpham199x <binhpham199x@gmail.com>
2024-05-31 21:25:04 +0200

    feat: add about sub page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+	return (
+		<div className="max-w-6xl mx-auto p-3 space-y-4">
+			<h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic
+				ipsum fuga voluptatem iure dicta. Fugiat molestiae incidunt
+				inventore itaque voluptatem vel dolor possimus fuga voluptate
+				harum. Ea soluta aperiam enim.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Voluptatum minima quibusdam veritatis maiores, error laudantium
+				explicabo ratione eligendi dicta. Illo dicta consequuntur veniam
+				quam ut unde eos quaerat assumenda voluptatibus!
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+				aspernatur, error accusamus dolores ipsa molestiae architecto
+				optio doloribus fuga! Error accusantium deserunt dolorem
+				voluptatem et quisquam tempore provident rerum nostrum!
+			</p>
+		</div>
+	);
+}
