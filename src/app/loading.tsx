e044614ce7ecdfa7aa515fa8b58433451904c06b
binhpham199x <binhpham199x@gmail.com>
2024-06-02 11:29:26 +0200

    feat: adjust loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 
 const loading = () => {
   return (
-    <div className="flex justify-center">
-        <img src="spinner.svg" alt="loading..."/>
+    <div className="flex justify-center mt-20">
+        <img className="h-15" src="spinner.svg" alt="loading..."/>
     </div>
   )
 }
